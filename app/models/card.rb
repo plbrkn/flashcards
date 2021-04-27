0a87234fc574f97fc08b85e0f921e5d58af24549
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-04-27 21:26:20 +0800

    add card model validates

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,7 @@
 
 # Card
 class Card < ApplicationRecord
+  validates :original_text, presence: true
+  validates :original_text, length: { in: 1..128 }
+  validates :translated_text, length: { in: 1..128 }, allow_blank: true
 end
