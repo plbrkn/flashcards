d3aceebabc256ebadc690eea88919ed5d0d072d2
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-04-28 19:02:40 +0800

    add test card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:card) { build(:card) }
+
+  it 'create valid card' do
+    expect(build(:card).save).to eq true
+  end
+
+  it 'create invalid card' do
+    expect(build(:card, original_text: nil).save).to eq false
+  end
+
+  after(:all) { Card.destroy_all }
 end
