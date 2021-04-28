a3a7028fea6b856a7fd3f5a0cd9d5c30114c7882
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-04-28 19:01:42 +0800

    add gem factory_bot

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'webpacker', '~> 4.0'
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails', '~> 5.0'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'support/factory_bot'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
