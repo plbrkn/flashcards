731a2176c9e16698081ea0b1eed4cdf5fa61cab2
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-05-01 17:10:22 +0800

    add CRUD card

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   root 'wellcome#wellcome'
 
-  resources :cards, only: :index
+  resources :cards
 end
--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -4,6 +4,5 @@ FactoryBot.define do
   factory :card do
     original_text { 'some text' }
     translated_text { 'какой то текст' }
-    review_date { Time.current }
   end
 end
